39fa3cc4902d4c2494878e74e0d09aa7ea3e85fa
Ayush Tripathi <ayushtripathi@Ayushs-MacBook-Pro.local>
2023-10-29 00:05:18 -0700

    created the template to recieve job details, and created a new hume output that gives facs

--- hume-api/hume-api-template.py
+++ hume-api/hume-api-template.py
@@ -2,13 +2,13 @@ import requests
 
 url = "https://api.hume.ai/v0/batch/jobs"
 
-payload = "{\"models\":{\"face\":{\"fps_pred\":1,\"prob_threshold\":0.9,\"identify_faces\":false,\"min_face_size\":60,\"facs\":{},\"descriptions\":{},\"save_faces\":false},\"burst\":{},\"prosody\":{\"granularity\":\"utterance\",\"window\":{\"length\":4,\"step\":1}},\"language\":{\"granularity\":\"word\"}},\"transcription\":{\"language\":null,\"identify_speakers\":false,\"confidence_threshold\":0.5},\"urls\":[\"https://drive.google.com/file/d/1VwKvxeKYGuXen11_rT23O3q3rES7CjDO/view?usp=sharing\"],\"notify\":false}"
-headers = {
-    "accept": "application/json; charset=utf-8",
-    "content-type": "application/json; charset=utf-8",
-    "X-Hume-Api-Key": "9BAoszAhvQSgWLIttRJHlBJRHavk4NWOzfZQUTrDSATB5RFu"
-}
+files = { "file": ("How%20Wind%20Energy%20Could%20Power%20Earth%20...%2018%20Times%20Over%20_%20Dan%20J%C3%B8rgensen%20_%20TED%20Countdown.mp4", open("How%20Wind%20Energy%20Could%20Power%20Earth%20...%2018%20Times%20Over%20_%20Dan%20J%C3%B8rgensen%20_%20TED%20Countdown.mp4", "rb"), "video/mp4") }
+payload = { "json": "{\"models\":{\"face\":{\"fps_pred\":1,\"prob_threshold\":0.9,\"identify_faces\":false,\"min_face_size\":60,\"facs\":{},\"descriptions\":{},\"save_faces\":false},\"burst\":{},\"prosody\":{\"granularity\":\"utterance\",\"window\":{\"length\":4,\"step\":1}},\"language\":{\"granularity\":\"word\"}},\"transcription\":{\"language\":null,\"identify_speakers\":false,\"confidence_threshold\":0.5},\"notify\":false}" }
+headers = {"accept": "application/json"}
 
-response = requests.post(url, data=payload, headers=headers)
+response = requests.post(url, data=payload, files=files, headers=headers)
 
 print(response.text)
+
+
+{"models":{"face":{"fps_pred":1,"prob_threshold":0.9,"identify_faces":false,"min_face_size":60,"facs":{},"descriptions":{},"save_faces":false},"burst":{},"prosody":{"granularity":"utterance","window":{"length":4,"step":1}},"language":{"granularity":"word"}},"transcription":{"language":null,"identify_speakers":false,"confidence_threshold":0.5},"notify":false}
