cb850934ac56f46e0c5ee06e484169a255a7127b
Arjun Ashok <arjun3.ashok@gmail.com>
2023-10-28 23:57:38 -0700

    more setup for the hume api wrapping

---
+++ hume-api/hume-api.py
@@ -0,0 +1,73 @@
+"""
+    Functionality for wrapping the hume-api calls in an easy & modularized way.
+"""
+
+# ----------------- Environment Setup ----------------- #
+import requests
+import json
+import asyncio
+
+
+# ----------------- API Functionality ----------------- #
+"""
+    Sets the default payload characteristics for the function.
+"""
+def default_payload() -> dict:
+    # default params
+    params_to_define = [
+        "models",
+        "transcription",
+        "notify"
+    ]
+    param_values = [
+        {
+            "face": {
+                "fps_pred": 1,
+                "prob_threshold": 0.9,
+                "identify_faces": False,
+                "min_face_size": 60,
+                "facs": {},
+                "descriptions": {},
+                "save_faces": False
+            }
+            "burst": {},
+            "prosody": {
+                "granularity": "utterance",
+                "window": {
+                    "length": 4,
+                    "step": 1
+                }
+            },
+            "language": {
+                "granularity": "word"
+            }
+        },
+        {
+            "language": null,
+            "identify_speakers": false,
+            "confidence_threshold": 0.5
+        },
+        False
+    ]
+    
+    return dict(zip(params_to_define, param_values))
+
+
+
+"""
+    Pushes the request to HUME and returns the [processed] output.
+"""
+async def gen_hume_analysis(params: dict):
+    # setup
+    url = "https://api.hume.ai/v0/batch/jobs"
+
+    payload = default_payload()
+    payload = json.dumps(payload)
+
+    headers = {
+        "accept": "application/json; charset=utf-8",
+        "content-type": "application/json; charset=utf-8",
+        "X-Hume-Api-Key": f"{params[API_KEY]}"
+    }
+
+    await response = requests.post(url, data=payload, headers=headers)
